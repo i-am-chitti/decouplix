3af18af513631f689cb59b5ce803c0dcc6975989
Deepak Kumar <deepak.kumar852182@gmail.com>
2026-06-22 15:33:12 +0530

    build: add dev and watch script aliases to package.json

--- assets/admin.asset.php
+++ assets/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '14f7deec3f99ed67996a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'a5c40c6a00047cd98f8b');
