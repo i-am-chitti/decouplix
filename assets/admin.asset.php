c382ff26da030d8db3385e0d19a0ef7afc6f7fee
Deepak Kumar <deepak.kumar852182@gmail.com>
2026-06-22 15:00:00 +0530

    style: remove important overrides by using specific CSS class chains

--- assets/admin.asset.php
+++ assets/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '1cf76106d32be001122d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '14f7deec3f99ed67996a');
